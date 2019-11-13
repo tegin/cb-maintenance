c5c83e860d667705e5d47e5c8784164d3b1e1d72
Jaime Arroyo <jaime.arroyo@creublanca.es>
2019-11-13 16:34:24 +0100

    [11.0][ADD] cb_maintenance

---
+++ cb_maintenance/models/maintenance_stage.py
@@ -0,0 +1,12 @@
+# Copyright 2019 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class MaintenanceStage(models.Model):
+
+    _inherit = 'maintenance.stage'
+
+    requires_technician = fields.Boolean('Requires Technician')
+    # TODO: Hablar de por que lo hacemos :)
