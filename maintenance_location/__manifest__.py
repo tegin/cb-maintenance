394d1a8691819a5a0ecb6533e9ee7bd45d6ea00e
Jaime Arroyo <jaime.arroyo@creublanca.es>
2019-11-15 12:54:23 +0100

    [11.0][ADD] maintenance_location

---
+++ maintenance_location/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2019 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Maintenance Location',
+    'summary': """
+        Define a location system for maintenance""",
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Creu Blanca,Odoo Community Association (OCA)',
+    'website': 'www.creublanca.es',
+    'depends': [
+        'maintenance'
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/maintenance_request.xml',
+        'views/maintenance_location.xml',
+    ],
+}
