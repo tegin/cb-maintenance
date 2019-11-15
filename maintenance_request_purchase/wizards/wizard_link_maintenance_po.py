2a00b13f9c9cd22e298a2c943ee375628723a9a8
Jaime Arroyo <jaime.arroyo@creublanca.es>
2019-11-15 12:54:44 +0100

    [11.0][ADD] maintenance_request_purchase

---
+++ maintenance_request_purchase/wizards/wizard_link_maintenance_po.py
@@ -0,0 +1,36 @@
+# Copyright 2019 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class WizardLinkMaintenancePo(models.TransientModel):
+
+    _name = 'wizard.link.maintenance.po'
+
+    maintenance_request_id = fields.Many2one('maintenance.request')
+    purchase_order_ids = fields.Many2many(
+        'purchase.order', string='Purchase Orders'
+    )
+
+    @api.multi
+    def link_po(self):
+        self.ensure_one()
+        new_purchase_orders = [
+            (4, po_id) for po_id in self.purchase_order_ids.ids
+        ]
+        self.maintenance_request_id.write({
+            'purchase_order_ids': new_purchase_orders
+        })
+        return {'type': 'ir.actions.act_window_close'}
+
+    @api.multi
+    def create_po(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'New',
+            'res_model': 'purchase.order',
+            'view_mode': 'form,tree',
+            'context': {'maintenance_request': self.maintenance_request_id.id}
+        }
