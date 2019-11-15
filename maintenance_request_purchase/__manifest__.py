2a00bf629b4b810d2f51023f77d7af690fbf4555
Jaime Arroyo <jaime.arroyo@creublanca.es>
2019-11-15 13:23:32 +0100

    [11.0][ADD] maintenance_request_purchase

---
+++ maintenance_request_purchase/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2019 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Maintenance Request Purchase',
+    'summary': """
+        Allows you to link PO with maintenance requests""",
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Creu Blanca,Odoo Community Association (OCA)',
+    'website': 'www.creublanca.es',
+    'depends': [
+        'maintenance',
+        'purchase',
+    ],
+    'data': [
+        'wizards/wizard_link_maintenance_po.xml',
+        'views/maintenance_request.xml',
+        'views/purchase_order_views.xml',
+    ],
+}
