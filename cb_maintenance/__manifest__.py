22d4cf9ac3a296a934df8335ba788686f6719c93
Enric Tobella <etobella@creublanca.es>
2019-11-20 18:55:06 +0100

    [ADD] maintenante_team_hierarchy

--- cb_maintenance/__manifest__.py
+++ cb_maintenance/__manifest__.py
@@ -2,23 +2,24 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Cb Maintenance',
-    'summary': """
+    "name": "Cb Maintenance",
+    "summary": """
         CB maintenance base""",
-    'version': '11.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Creu Blanca',
-    'website': 'www.creublanca.es',
-    'depends': [
-        'maintenance_plan',
-        'maintenance_location',
-        'web_widget_child_selector',
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Creu Blanca",
+    "website": "www.creublanca.es",
+    "depends": [
+        "maintenance_plan",
+        "maintenance_location",
+        "web_widget_child_selector",
+        "maintenance_team_hierarchy",
     ],
-    'data': [
-        'views/maintenance_stage.xml',
-        'views/maintenance_team.xml',
-        'wizards/wizard_change_maintenance_team.xml',
-        'views/maintenance_request.xml',
-        'wizards/wizard_create_maintenance_request.xml',
+    "data": [
+        "views/maintenance_stage.xml",
+        "views/maintenance_team.xml",
+        "wizards/wizard_change_maintenance_team.xml",
+        "views/maintenance_request.xml",
+        "wizards/wizard_create_maintenance_request.xml",
     ],
 }
