364e7d3c507aa232902b3ee2aaa33fd52f3d1125
Jaime Arroyo <jaime.arroyo@creublanca.es>
2019-12-09 09:48:47 +0100

    [11.0][ADD] cb_maintenance_migration

---
+++ cb_maintenance_migration/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2019 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Cb Maintenance Migration",
+    "description": """
+        Migration info""",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Creu Blanca",
+    "website": "www.creublanca.es",
+    "depends": ["maintenance"],
+    "data": [
+        "views/maintenance_equipment.xml",
+        "views/maintenance_request.xml",
+    ],
+}
