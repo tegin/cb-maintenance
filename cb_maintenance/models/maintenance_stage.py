6891cf38297328665d6ed0a75d2448391bbf1351
Jaime Arroyo <jaime.arroyo@creublanca.es>
2019-12-09 18:17:00 +0100

    [11.0][FIX] cb_maintenance: Minor Fixes

--- cb_maintenance/models/maintenance_stage.py
+++ cb_maintenance/models/maintenance_stage.py
@@ -1,7 +1,7 @@
 # Copyright 2019 Creu Blanca
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 from .maintenance_request import REQUEST_STATES
 
 
@@ -10,3 +10,10 @@ class MaintenanceStage(models.Model):
     _inherit = "maintenance.stage"
 
     state = fields.Selection(selection=REQUEST_STATES)
+
+    done = fields.Boolean(compute="_compute_stage_done", store=True)
+
+    @api.depends("state")
+    def _compute_stage_done(self):
+        for record in self:
+            record.done = record.state == "closed"
