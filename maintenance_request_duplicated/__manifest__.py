0b8be9c3ea5a745114e47e14896f301882fd1c59
Jaime Arroyo <jaime.arroyo@creublanca.es>
2019-12-11 16:40:09 +0100

    [11.0][ADD] maintenance_request_duplicated

---
+++ maintenance_request_duplicated/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2019 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Maintenance Request Duplicated",
+    "summary": """
+        This module allows you to manage duplicated requests""",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Creu Blanca,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/maintenance",
+    "depends": ["cb_maintenance_project"],
+    "data": [
+        "data/maintenance_stage_data.xml",
+        "wizards/wizard_request_duplicated.xml",
+        "views/maintenance_request.xml",
+    ],
+}
---
+++ maintenance_request_duplicated/wizards/wizard_request_duplicated.py
@@ -0,0 +1,30 @@
+# Copyright 2019 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
+
+
+class WizardRequestDuplicated(models.TransientModel):
+
+    _name = "wizard.request.duplicated"
+
+    duplicated_request = fields.Many2one(comodel_name="maintenance.request")
+    original_request = fields.Many2one(
+        comodel_name="maintenance.request", required=True
+    )
+
+    @api.multi
+    def mark_as_duplicated(self):
+        self.ensure_one()
+        if not self.duplicated_request:
+            raise ValidationError(_("Active Request not found"))
+
+        self.original_request.write(
+            {"duplicated_ids": [(4, self.duplicated_request.id)]}
+        )
+        stage_id = self.env.ref(
+            "maintenance_request_duplicated.duplicated_stage"
+        )
+        self.duplicated_request.write({"stage_id": stage_id.id})
+        return {"type": "ir.actions.act_window_close"}
