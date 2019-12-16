38a623309eac81f7f2b6856930b5c427f6b0f8ec
Jaime Arroyo <jaime.arroyo@creublanca.es>
2019-12-16 18:16:58 +0100

    [11.0][ADD] cb_maintenance_security

---
+++ cb_maintenance_security/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2019 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Cb Maintenance Security",
+    "description": """
+        Security""",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Creu Blanca",
+    "website": "www.creublanca.es",
+    "depends": ["cb_maintenance"],
+    "data": [
+        "data/maintenance_data.xml",
+        "security/maintenance_security.xml",
+        "views/menu_items.xml",
+    ],
+}
