7fb71e03c9d95c4d1d0d66e369c00cbfe2eec12e
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-01-22 16:59:04 +0100

    [11.0][IMP] Add location to plans

--- maintenance_location/models/maintenance_equipment.py
+++ maintenance_location/models/maintenance_equipment.py
@@ -9,3 +9,13 @@ class MaintenanceEquipment(models.Model):
     _inherit = "maintenance.equipment"
 
     location_id = fields.Many2one("maintenance.location")
+
+    def _prepare_request_from_plan(
+        self, maintenance_plan, next_maintenance_date
+    ):
+        res = super()._prepare_request_from_plan(
+            maintenance_plan, next_maintenance_date
+        )
+        location = maintenance_plan.location_id or self.location_id
+        res.update({"location_id": location.id})
+        return res
