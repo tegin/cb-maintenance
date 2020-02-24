b9bc05169958d95d9217fae2a051ad65bf89e531
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-02-24 13:46:59 +0100

    [12.0][MIG] cb_maintenance repo

--- base_remote_ocs/__manifest__.py
+++ base_remote_ocs/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Base Remote Ocs",
     "description": """
         Use remote and link to OCS""",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca",
     "website": "www.creublanca.es",
