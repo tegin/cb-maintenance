bc81dee939a0ee4931a241e0b6655f5349366ea5
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-08-17 09:28:41 +0200

    [12.0][FIX] purchase_view context smart button

--- maintenance_request_purchase/models/maintenance_request.py
+++ maintenance_request_purchase/models/maintenance_request.py
@@ -6,35 +6,39 @@ from odoo import api, fields, models
 
 class MaintenanceRequest(models.Model):
 
-    _inherit = 'maintenance.request'
+    _inherit = "maintenance.request"
 
     purchase_order_ids = fields.Many2many(
-        'purchase.order',
-        'maintenance_purchase_order',
-        'maintenance_request_id', 'purchase_order_id',
-        string='Purchase Orders'
+        "purchase.order",
+        "maintenance_purchase_order",
+        "maintenance_request_id",
+        "purchase_order_id",
+        string="Purchase Orders",
     )
     purchases_count = fields.Integer(
-        compute='_compute_purchases_count', store=True
+        compute="_compute_purchases_count", store=True
     )
 
-    @api.depends('purchase_order_ids')
+    @api.depends("purchase_order_ids")
     def _compute_purchases_count(self):
         for record in self:
             record.purchases_count = len(record.purchase_order_ids.ids)
 
     @api.multi
     def action_view_purchase(self):
-        action = self.env.ref(
-            'purchase.purchase_order_action_generic').read()[0]
+        action = self.env.ref("purchase.purchase_order_action_generic").read()[
+            0
+        ]
         if len(self.purchase_order_ids) > 1:
-            action['domain'] = [('id', 'in', self.purchase_order_ids.ids)]
-            action['views'] = [
-                (self.env.ref('purchase.purchase_order_tree').id, 'tree'),
-                (self.env.ref('purchase.purchase_order_form').id, 'form'),
+            action["domain"] = [("id", "in", self.purchase_order_ids.ids)]
+            action["views"] = [
+                (self.env.ref("purchase.purchase_order_tree").id, "tree"),
+                (self.env.ref("purchase.purchase_order_form").id, "form"),
             ]
         elif self.purchase_order_ids:
-            action['views'] = [
-                (self.env.ref('purchase.purchase_order_form').id, 'form')]
-            action['res_id'] = self.purchase_order_ids.id
+            action["views"] = [
+                (self.env.ref("purchase.purchase_order_form").id, "form")
+            ]
+            action["res_id"] = self.purchase_order_ids.id
+        action["context"] = {}
         return action
