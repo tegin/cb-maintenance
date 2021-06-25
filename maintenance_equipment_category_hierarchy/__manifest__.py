cec3bdfcd5877852671c0d3983cf8c9047b1d79a
Olga Marco <olga.marco@creublanca.es>
2021-06-25 08:55:13 +0200

    [MIG] maintenance_equipment_category_hierarchy: Migration to 13.0

--- maintenance_equipment_category_hierarchy/__manifest__.py
+++ maintenance_equipment_category_hierarchy/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Maintenance Equipment Category Hierarchy",
     "summary": """
         Equipment Categories Hierarchy""",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca",
     "website": "www.creublanca.es",
