088c9a058307dcd2d6ac6e6e373ec053b8fa4ea0
Olga Marco <olga.marco@creublanca.es>
2021-06-25 12:10:03 +0200

    [MIG] maintenance_request_custom_info: Migration to 13.0

--- maintenance_request_custom_info/__manifest__.py
+++ maintenance_request_custom_info/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Maintenance Request Custom Info",
     "summary": """
         Maintenance Request Custom Info""",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "www.creublanca.es",
