a79b0d16b30d72aa30fa63e7bf770435158472be
Núria Sancho <nuria.sancho@creublanca.es>
2022-11-08 13:03:35 +0100

    [MIG] cb_maintenance_project: Migration to 14.0

--- cb_maintenance_project/__manifest__.py
+++ cb_maintenance_project/__manifest__.py
@@ -5,9 +5,9 @@
     "name": "Cb Maintenance Project",
     "summary": """
         Expedientes de Mantenimiento""",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
-    "author": "Creu Blanca,Odoo Community Association (OCA)",
+    "author": "CreuBlanca,Odoo Community Association (OCA)",
     "website": "https://github.com/tegin/cb-maintenance",
     "depends": [
         "base_maintenance",
@@ -15,6 +15,7 @@
         "maintenance_request_purchase",
     ],
     "data": [
+        "security/ir.model.access.csv",
         "templates/assets.xml",
         "wizards/wizard_group_in_project.xml",
         "wizards/wizard_create_project_issue.xml",
