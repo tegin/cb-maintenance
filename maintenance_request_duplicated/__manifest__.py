1989183399e64505765f21363c7d84f918587af8
Núria Sancho <nuria.sancho@creublanca.es>
2022-11-10 16:26:51 +0100

    [IMP] maintenance_request_duplicated : black, isort, prettier

--- maintenance_request_duplicated/__manifest__.py
+++ maintenance_request_duplicated/__manifest__.py
@@ -8,7 +8,7 @@
     "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/maintenance",
+    "website": "https://github.com/tegin/cb-maintenance",
     "depends": ["cb_maintenance_project"],
     "data": [
         "data/maintenance_stage_data.xml",
