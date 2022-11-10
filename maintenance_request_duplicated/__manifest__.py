74302048c538ef1988ea4c7c9da95131d3a318ec
Núria Sancho <nuria.sancho@creublanca.es>
2022-11-10 16:37:13 +0100

    [14.0][MIG] maintenance_request_duplicated: Migration to 14.0

--- maintenance_request_duplicated/__manifest__.py
+++ maintenance_request_duplicated/__manifest__.py
@@ -5,12 +5,13 @@
     "name": "Maintenance Request Duplicated",
     "summary": """
         This module allows you to manage duplicated requests""",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
-    "author": "Creu Blanca,Odoo Community Association (OCA)",
+    "author": "CreuBlanca,Odoo Community Association (OCA)",
     "website": "https://github.com/tegin/cb-maintenance",
     "depends": ["cb_maintenance_project"],
     "data": [
+        "security/ir.model.access.csv",
         "data/maintenance_stage_data.xml",
         "wizards/wizard_request_duplicated.xml",
         "views/maintenance_request.xml",
