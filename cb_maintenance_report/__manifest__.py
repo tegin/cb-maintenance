ec12d54427e8765842412ec661b7734fe2ad57c0
Kevin Luna <kevin.luna@creublanca.es>
2022-11-10 16:27:07 +0100

    [IMP] cb_maintenance_report: black, isort, prettier

--- cb_maintenance_report/__manifest__.py
+++ cb_maintenance_report/__manifest__.py
@@ -8,7 +8,7 @@
     "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca",
-    "website": "www.creublanca.es",
+    "website": "https://github.com/tegin/cb-maintenance",
     "depends": ["cb_maintenance_project"],
     "data": ["report/maintenance_request_report.xml"],
 }
