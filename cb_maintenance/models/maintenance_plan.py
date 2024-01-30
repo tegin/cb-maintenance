ae87087ab3078065bf0e39d3b856f4a35974284d
Kevin Luna <kevin.luna@creublanca.es>
2024-01-30 16:56:52 +0100

    [14.0][IMP] cb_maintenance: add maneger_id in maintence_plan_form

--- cb_maintenance/models/maintenance_plan.py
+++ cb_maintenance/models/maintenance_plan.py
@@ -1,7 +1,7 @@
 # Copyright 2020 Creu Blanca
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class MaintenancePlan(models.Model):
@@ -12,3 +12,23 @@ class MaintenancePlan(models.Model):
         "res.partner", domain="[('is_maintenance_technician', '=', True)]"
     )
     category_id = fields.Many2one("maintenance.equipment.category")
+
+    manager_id = fields.Many2one("res.users", string="Manager", default=False)
+
+    maintenance_team_id_member_ids = fields.Many2many(
+        "res.users",
+        compute="_compute_maintenance_team_id_member_ids",
+    )
+
+    @api.depends("maintenance_team_id")
+    def _compute_maintenance_team_id_member_ids(self):
+        for record in self:
+            if record.maintenance_team_id:
+                users = (
+                    self.env["maintenance.team"]
+                    .search([("id", "child_of", record.maintenance_team_id.id)])
+                    .mapped("member_ids")
+                )
+                record.maintenance_team_id_member_ids = [(6, 0, users.ids)]
+            else:
+                record.maintenance_team_id_member_ids = False
