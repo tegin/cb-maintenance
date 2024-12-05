da064be4f5085da00bb5bbbf24aeb12ed693fe52
Kevin Luna <kevin.luna@creublanca.es>
2024-12-05 12:30:01 +0100

    [MIG] cb_maintenance: Migration to 16.0

--- cb_maintenance_security/__manifest__.py
+++ cb_maintenance_security/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Cb Maintenance Security",
     "summary": """
         Security""",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "CreuBlanca",
     "website": "https://github.com/tegin/cb-maintenance",
